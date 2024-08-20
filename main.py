1f99f696132caa13288433f199b0066b70b7b735
humwer <dev.humwer@gmail.com>
2024-08-20 23:58:45 +0300

    Update #24 Delete robots

--- main.py
+++ main.py
@@ -66,11 +66,6 @@ def ok():
     return 'OK'
 
 
-@app.route("/robots.txt", methods=["GET"])
-def robots():
-    return send_from_directory(app.root_path, "robots.txt", mimetype='text/plain')
-
-
 @app.route("/search", methods=["POST"])
 def search():
     context_login = False
