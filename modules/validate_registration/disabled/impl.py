77c0736b4501f87ebc3dfa2155d077f4e1db6e61
Oleg Mazurok <lgmzrk@gmail.com>
2025-12-04 10:08:02 +0300

    Added module with disabled registration for future modules of login

---
+++ modules/validate_registration/disabled/impl.py
@@ -0,0 +1,9 @@
+from utils import *
+
+'''
+Отключена регистрация (для модулей работы со входом)
+'''
+
+
+def validate_registration(login: str, password: str, confirm_password: str) -> tuple:
+    return 0, 'Идут технические работы! Регистрация временно отключена.'
